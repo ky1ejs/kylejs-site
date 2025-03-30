0b53fc48090ef7ec12e315ad734b82b75bc97a22
Kyle Satti <k@kylejs.dev>
2025-03-30 19:35:57 -0400

    linter

--- components/Theme/ThemeButton.tsx
+++ components/Theme/ThemeButton.tsx
@@ -4,7 +4,26 @@ import { useTheme } from "@/components/Theme/ThemeProvider";
 import { useEffect, useState } from "react";
 
 const LoadingIndicator = () => (
-  <svg className="size-5 animate-spin text-muted" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
+  <svg
+    className="size-5 animate-spin text-muted"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      strokeWidth="4"
+    ></circle>
+    <path
+      className="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+    ></path>
+  </svg>
 );
 
 export default function ThemeButton() {
@@ -22,7 +41,6 @@ export default function ThemeButton() {
         return "🖥️";
     }
   };
-
 
   useEffect(() => {
     // workaround for Next.js hydration issue with reading theme from cookies
@@ -44,15 +62,17 @@ export default function ThemeButton() {
   }, []);
 
   return (
-    <div className="relative w-12 h-10 rounded-xl bg-background-secondary flex items-center justify-center hover:bg-background-200">
-      <div className={`${isMounted ? "opacity-0" : "opacity-100"} absolute z-10 transition-opacity duration-300`}>
+    <div className="relative flex h-10 w-12 items-center justify-center rounded-xl bg-background-secondary hover:bg-background-200">
+      <div
+        className={`${isMounted ? "opacity-0" : "opacity-100"} absolute z-10 transition-opacity duration-300`}
+      >
         <LoadingIndicator />
       </div>
       {isMounted && (
         <>
           <button
             id="theme-button"
-            className="w-full h-full z-20"
+            className="z-20 h-full w-full"
             onClick={() => setIsOpen(!isOpen)}
           >
             <span className="text-lg">{emojiForTheme(theme)}</span>
@@ -77,7 +97,8 @@ export default function ThemeButton() {
               ))}
             </div>
           )}
-        </>)}
+        </>
+      )}
     </div>
   );
 }
