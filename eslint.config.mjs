9fd921ead2df85ccf19c158ae7b39223942bfbc5
Kyle Satti <k@kylejs.dev>
2026-01-08 08:09:15 -0500

    upgrade to nextjs 16

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import prettier from "eslint-plugin-prettier";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: [
+        ...nextCoreWebVitals,
+        ...compat.extends("plugin:@typescript-eslint/recommended"),
+        ...compat.extends("eslint:recommended"),
+        ...compat.extends("prettier")
+    ],
+
+    plugins: {
+        prettier,
+    },
+
+    rules: {
+        "prefer-const": "error",
+        "prettier/prettier": "error",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "error",
+    },
+}]);
